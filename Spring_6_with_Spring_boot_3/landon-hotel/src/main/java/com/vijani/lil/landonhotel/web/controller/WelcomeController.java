2392a3dbbcbb3c06c5017d22183790723f026f4f
vijanipiyawardana <vijanipiyawardana@gmail.com>
2024-02-17 21:20:31 +0100

    First controller done

---
+++ Spring_6_with_Spring_boot_3/landon-hotel/src/main/java/com/vijani/lil/landonhotel/web/controller/WelcomeController.java
@@ -0,0 +1,28 @@
+package com.vijani.lil.landonhotel.web.controller;
+
+import io.micrometer.common.util.StringUtils;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String getWelcome(@RequestParam(value = "name", required = false) String name) {
+        String greeting = "Hello ";
+        if (StringUtils.isNotBlank(name)) {
+            greeting = greeting + name;
+        } else {
+            greeting = greeting + "world";
+        }
+        return "<h1>" + greeting + "</h1>";
+    }
+
+}
